e282870c2b155bc724fc0e152e7dc23c4c941f24
liang <l2269076494@163.com>
2026-05-21 17:30:07 +0800

    Signed-off-by: liang <l2269076494@163.com>修改测试用例

--- SmartDoorLock/HAL/HAL_GPIO.h
+++ SmartDoorLock/HAL/HAL_GPIO.h
@@ -1,7 +1,7 @@
 #ifndef __HAL_GPIO_H
 #define __HAL_GPIO_H
 //Ê¹ÄÜLED²âÊÔº¯Êý
-#define HAL_MCU_LED_TEST_ENABLE
+//#define HAL_MCU_LED_TEST_ENABLE
 void HAL_GPIO_Init();
 
 #ifdef HAL_MCU_LED_TEST_ENABLE
